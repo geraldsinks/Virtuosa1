221d3f574bb423f4e8a02ef4ad3d8fcaa495f954
Zenflow <noreply@zencoder.ai>
2026-03-17 10:42:38 +0200

    added server serving of image

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ app.use(cors( {
 }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client')));
+app.use('/uploads', express.static(path.join(__dirname, '../client/uploads')));
 
 // Test endpoint
 app.get('/api/auth/test', (req, res) => {
