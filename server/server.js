076e4698bb9ec67aab49fd940b4693b36166cc98
Zenflow <noreply@zencoder.ai>
2026-04-25 17:06:13 +0300

    server

--- server/server.js
+++ server/server.js
@@ -68,7 +68,6 @@ const User = require('./models/User');
 const Product = require('./models/Product');
 const Notification = require('./models/Notification');
 const SellerApplication = require('./models/SellerApplication');
-const ProductGrid = require('./models/ProductGrid');
 require('dotenv').config({ path: path.join(__dirname, 'config/.env') });
 
 // Environment and path configuration
